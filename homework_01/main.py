1c52a4524cd7fa652cbeab349b84c0e2847fd29d
Pavel Kudrevatykh <jazex@mail.ru>
2022-01-10 11:06:48 +0500

    Задание выполнено

--- homework_01/main.py
+++ homework_01/main.py
@@ -27,6 +27,36 @@ PRIME = "prime"
 
 
 def filter_numbers():
+
+
+# функция, которая фильтрует нечетные числа
+def filter_even_num(in_num):
+    if (in_num % 2) == 0:
+        return True
+    else:
+        return False
+
+# функция, которая фильтрует четные числа
+def filter_odd_num(args):
+    if (args % 2) == 0:
+        return False
+    else:
+        return True
+
+# функция, которая фильтрует натуральные числа
+def is_prime(in_num):
+    k = 0
+    if in_num >= 2:
+        for i in range(2, in_num // 2 + 1):
+            if (in_num % i == 0):
+                k = k + 1
+        if (k <= 0):
+            return True
+        else:
+            return False
+    else:
+        return False
+
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -37,3 +67,18 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+def filter_numbers(*args, t):
+      if t is ODD:
+        out_filter = filter(filter_odd_num, args)
+        print("Отфильтрованный список: ", list(out_filter))
+      elif t is EVEN:
+          out_filter = filter(filter_even_num, args)
+          print("Отфильтрованный список: ", list(out_filter))
+      elif t is PRIME:
+          out_filter = filter(is_prime, args)
+          print("Отфильтрованный список: ", list(out_filter))
+
+
+filter_numbers(2, 3, 4, 5, t=EVEN)
+
